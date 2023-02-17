77298a86ebe143cc33db8de2dcda460a33d19a9d
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-17 17:32:24 +0545

    1 file modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const api= require("./Api.js")
 
 app.use(express.static('static'));
 
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
  app.get('/about',(req,res)=>{
   res.sendFile(path.resolve('pages/about.html'))
 })
+api.getMenu
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
