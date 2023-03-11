28d675836ca12e19c8056926e378c87a67e79821
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-11 12:06:08 +0545

    1 file modified

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const port = 3010;
 const path = require('path');
 const shop =require("./Api.js")
 const Router =require("./routes.js")
+
+app.use(express.json())
 app.use(express.static('static'));
 
-
-app.use("/",Router)
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
-});
-app.post("/re",(req,res)=>{
-  res.end(req.e.target.value)
-})
- app.get('/about',(req,res)=>{
-  res.sendFile(path.resolve('pages/about.html'))
+app.get("/",(req,res)=>{
+  res.sendFile(path.resolve("pages/Signup.html"))
 })
 
-app.use("/shop",shop);
+app.post("/Signup",(req,res)=>{
+  console.log(req.body.username)
+  res.end("welcome to signup page")
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
