cb8461d77c871af20cc02e90a9b679270f95cd4d
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-12 18:15:08 +0545

    2 files modified

--- routes.js
+++ routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const Router = express.Router();
 const path = require('path');
-
+const bodyParser=require("body-parser")
 Router.get('/Signup', (req, res) => {
   res.sendFile(path.resolve('pages/Signup.html'));
 
 });
+Router.use("/Signup",bodyParser.urlencoded({extended:true}))
 Router.post('/Signup', (req, res) => {
-  console.log(req.body);
+  console.log(req.body.username);
   
 });
 Router.get('/',(req,res)=>{
