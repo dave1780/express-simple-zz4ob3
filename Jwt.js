2c5a8857c38ea7e623f900788c891c68e646c87e
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-14 11:26:20 +0545

    1 file modified

--- Jwt.js
+++ Jwt.js
@@ -1,2 +1,8 @@
-const rwt =require("jsonwebtoken")
-const randomKey=require("random-key")
+const jwt =require("jsonwebtoken")
+const rand=require("random-key")
+const Randgenerate= rand.generate(32)
+const stringrand= Randgenerate.toString();
+console.log(stringrand,typeof(stringrand))
+
+const token= jwt.sign({foo:"magoo"},stringrand)
+console.log(token)
